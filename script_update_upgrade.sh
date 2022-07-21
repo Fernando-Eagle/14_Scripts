7750c47164b73a2ff71a3010ddd9583e59e14a16
Fernando-Eagle <fernandodesousa777@gmail.com>
2022-07-21 14:51:46 -0300

    Add files via upload
    Update for scripts of update & upgrade including flatpak packets.

--- script_update_upgrade.sh
+++ script_update_upgrade.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#Atualiza pacotes apt, snap e limpará a memória swap
+#Fernando "Eagle" De Sousa
+#fernandodesousa.wordpress.com
+#16/07/2022
+#https://https://github.com/Fernando-Eagle/shell_scripts
 
 #Variáveis de cores
 vermelho="\033[1;31m"
@@ -11,15 +16,22 @@ fundopurple="\033[45;1;37m"
 NORMAL="\033[m"
 
 clear
-  echo -e "${fundoazul}Este script irá efetuar a atualização dos pacotes Apt, Snap e limpará a memória Swap ${NORMAL}"
+  echo -e "${fundoazul}Este script irá efetuar a atualização dos pacotes Apt, Snap, Flatpak e limpará a memória Swap ${NORMAL}"
   echo -e "${fundoazul}------------------------------------------------------------------------------------ ${NORMAL}"
-  echo -e "${fundovermelho}Iniciando a atualização dos pacotes apt:${NORMAL}"; sleep 2
-sudo apt update && sudo apt list --upgradable && sudo apt upgrade && sudo apt autoclean && sudo apt autoremove
+  echo -e "${fundovermelho}Iniciando a atualização dos pacotes apt:${NORMAL}"; sleep 5
+sudo apt update && sudo apt list --upgradable && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoclean && sudo apt autoremove
 
 clear
   echo -e "${fundoazul}A atualização de pacotes apt efetuada com sucesso!${NORMAL}"
-  echo -e "${fundovermelho}Iniciando a atualização dos pacotes snap:${NORMAL}"; sleep 2
-sudo snap list && sudo snap refresh
+  echo -e "${fundovermelho}Iniciando a atualização dos pacotes snap:${NORMAL}";
+sudo snap refresh && sudo snap list
+sleep 5
+
+clear
+  echo -e "${fundoazul}A atualização de pacotes snap efetuada com sucesso!${NORMAL}"
+  echo -e "${fundovermelho}Iniciando a atualização dos pacotes flatpak:${NORMAL}";
+flatpak update && flatpak list
+sleep 5
 
 clear
   echo -e "${fundoazul}A atualização de pacotes snap efetuada com sucesso!${NORMAL}"
--- script_update_upgrade_options.sh
+++ script_update_upgrade_options.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#Atualiza pacotes apt, snap e limpará a memória swap com opções
+#Fernando "Eagle" De Sousa
+#fernandodesousa.wordpress.com
+#16/07/2022
+#https://https://github.com/Fernando-Eagle/shell_scripts
 
 #Variáveis de cores
 vermelho="\033[1;31m"
@@ -18,18 +23,20 @@ Main() {
   echo
   echo "[1] Atualizar pacotes Apt"
   echo "[2] Atualizar pacotes Snap"
-  echo "[3] Limpar memória Swap"
-  echo "[4] Reinicializar"
-  echo "[5] Sair"
+  echo "[3] Atualizar pacotes Flatpak"
+  echo "[4] Limpar memória Swap"
+  echo "[5] Reinicializar"
+  echo "[6] Sair"
   echo
   echo -n -e "${fundoazul}Qual a opção desejada? ${NORMAL}"
   read opcao
   case $opcao in
     1) Atualiza_apt ;;
     2) Atualiza_snap ;;
-    3) Limpa_swap ;;
-    4) Reiniciar_sistema ;;
-    5) clear; exit ;;
+    3) Atualiza_flatpak ;;
+    4) Limpa_swap ;;
+    5) Reiniciar_sistema ;;
+    6) clear; exit ;;
     *) echo -e "${fundovermelho}Opção desconhecida.${NORMAL}"; sleep 2 ; Main ;;
   esac
 }
@@ -37,7 +44,7 @@ Main() {
 Atualiza_apt() {
 clear
   echo -e "${fundovermelho}Iniciando a atualização dos pacotes apt:${NORMAL}"; sleep 2
-sudo apt update && sudo apt list --upgradable && sudo apt upgrade && sudo apt autoclean && sudo apt autoremove
+sudo apt update && sudo apt list --upgradable && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoclean && sudo apt autoremove
   echo -e "${fundoazul}A atualização de pacotes apt efetuada com sucesso!${NORMAL}"
   echo -n -e "${amarelo}Deseja retornar ao Menu Principal (S/n)${NORMAL}"
 read voltar
@@ -53,6 +60,20 @@ clear
   echo -e "${fundovermelho}Iniciando a atualização dos pacotes snap:${NORMAL}"; sleep 2
 sudo snap list && sudo snap refresh
   echo -e "${fundoazul}A atualização de pacotes snap efetuada com sucesso!${NORMAL}"
+  echo -n -e "${amarelo}Deseja retornar ao Menu Principal (S/n)${NORMAL}"
+read voltar
+  if [ $voltar = "S" ]; then
+    Main
+  elif [ $voltar = "n" ]; then
+    clear; exit
+  fi
+  }
+
+Atualiza_flatpak() {
+clear
+  echo -e "${fundovermelho}Iniciando a atualização dos pacotes flatpak:${NORMAL}"; sleep 2
+flatpak update && flatpak list
+  echo -e "${fundoazul}A atualização de pacotes snap flatpak efetuada com sucesso!${NORMAL}"
   echo -n -e "${amarelo}Deseja retornar ao Menu Principal (S/n)${NORMAL}"
 read voltar
   if [ $voltar = "S" ]; then
