8a5ffd5b5c21d6b5e02b7e95650d48123003accc
Fernando-Eagle <fernandodesousa777@gmail.com>
2024-05-11 09:47:41 -0300

    Add files via upload

---
+++ script_install_openvpn.sh
@@ -0,0 +1,22 @@
+# A sintaxe é: openvpn --config client.ovpn
+# Atualizando todos os pacotes existentes na estação 
+  351  ~/Downloads/Shell/script_update_upgrade.sh 
+  352  cd ~
+  353  ls
+  354  ~/Downloads/Shell/script_update_upgrade.sh 
+  355  cd Downloads/
+  356  ls
+  357  cd Openvpn/
+  358  la
+  359  openvpn --version
+  360  ls
+  361  openvpn --config afvsousa.ovpn
+  362  sudo apt-get network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-vpnc
+  363  sudo apt-cache search network
+  364  sudo apt-cache search network-manager-op
+  365  sudo apt-get install network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-vpnc
+  366  openvpn --config afvsousa.ovpn
+  367  sudo apt-get install network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-vpnc
+  368  ~/Downloads/Shell/script_update_upgrade.sh 
+  369  sudo reboot
+
